10052787a7881f89726d8df347c3ada3cfa60633
Fahrizalky <fahrizalkusumayuda2003@gmail.com>
2024-11-23 14:57:23 +0700

    menamabhkan id,dan nama dalam pupuk

--- route/Pupuk.js
+++ route/Pupuk.js
@@ -2,17 +2,13 @@ import express, { Router } from "express";
 const router = express.Router();
 const pupuk = [
     {
-        merk: "Toyota",
-        model: "Avanza",
-        warna: "Hitam",
-        tahun: 2021,
+        id: 2,
+        nama: "Urea",
+        
+        
+
     },
-    {
-        merk: "Porsche",
-        model: "sport",
-        warna: "Kuning",
-        tahun: 2024,
-    },
+   
 ];
 
 router.get("/", (req, res) => {
