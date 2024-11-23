2d00b8027b02a1fca88656e74a393cd1bc530e9e
Fahrizalky <fahrizalkusumayuda2003@gmail.com>
2024-11-23 15:13:34 +0700

    menambahkan import bibitRoute

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 import express from "express";
 import bodyParser from "body-parser";
-import mobilRoute from "./routes/Mobil.js";
-import sepedaRoute from "./routes/Sepeda.js";
-import sepatuRoute from "./routes/Sepatu.js";
-import hpRoute from "./routes/HP.js";
-import kaosRoute from "./routes/Kaos.js";
+import bibitRoute from "./routes/bibitTanaman.js";
+
+
 
 const app = express();
 const PORT = 3000;
@@ -12,15 +10,12 @@ const PORT = 3000;
 app.use(bodyParser.json());
 
 // Menambahkan setiap rute dengan variabel yang unik
-app.use("/mobil", mobilRoute);
-app.use("/sepeda", sepedaRoute);
-app.use("/sepatu", sepatuRoute);
-app.use("/hp", hpRoute);
-app.use("/kaos", kaosRoute);
+app.use("/Bibit", bibitRoute);
+app.use("/Pupuk", pupukRoute);
 
 app.get("/", (req, res) => {
     console.log(['GET ROUTE']);
-    res.send("Selamat Pagi");
+    res.send("Welcome Petani Nusantara");
 });
 
 app.listen(PORT, () =>
